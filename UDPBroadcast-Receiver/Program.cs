5c1acc19a65f57753c39297f2e6ffd8eb71c5b59
Filipsprojekter <45385618+Filipsprojekter@users.noreply.github.com>
2020-02-13 22:36:23 +0100

    Added several examples
    Changes contain a bunch of examples of different solutions to various assignments that you can meet at the exam. 
    They will not be directly applicable at the exam, but will be used to get a better understanding and will be used as part of the repetition material.

---
+++ UDPBroadcast-Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient UDPServer = new UdpClient(7001);
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 7001);
+
+            while (true)
+            {
+                Byte[] recieveBytes = UDPServer.Receive(ref remoteIpEndPoint);
+                string recievedData = Encoding.ASCII.GetString(recieveBytes);
+                string[] data = recievedData.Split("\n");
+                
+                foreach (var s in data)
+                {
+                    Console.WriteLine(s);
+                }
+
+
+
+
+            }
+        }
+    }
+}
